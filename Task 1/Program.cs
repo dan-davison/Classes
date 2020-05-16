e82d119607e15f1d6bda50a211ad64943520cf35
dan-davison <103009408@student.swin.edu.au>
2020-05-16 20:32:52 +1000

    Task Submission

---
+++ Task 1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Classes_Task_1_DanDavison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Instantiate a new Cookie
+            //Cookie 1
+            Colour C1 = new Colour(0, 128, 255);
+            Cookie ck1 = new Cookie (100,"Round", C1);
+            System.Console.WriteLine("Cookie 1");
+            System.Console.WriteLine("Weight: " + ck1.Weight + ", Shape: " + ck1.Shape + ", Colour: " + "Red:" + C1.Red + " Green:" + C1.Green + " Blue:" + C1.Blue);
+
+            System.Console.WriteLine();
+
+            //Cookie 2
+            Colour C2 = new Colour(0, 128, 255);
+            Cookie ck2 = new Cookie (120,"Sqaure", C2);
+            System.Console.WriteLine("Cookie 2");
+            System.Console.WriteLine("Weight: " + ck2.Weight + ", Shape: " + ck2.Shape + ", Colour: " + "Red:" + C2.Red + " Green:" + C2.Green + " Blue:" + C2.Blue);
+
+            System.Console.WriteLine();
+
+            //Cookie 3
+            Colour C3 = new Colour(255, 51, 51);
+            Cookie ck3 = new Cookie (150,"Round", C3);
+            System.Console.WriteLine("Cookie 3");
+            System.Console.WriteLine("Weight: " + ck3.Weight + ", Shape: " + ck3.Shape + ", Colour: " + "Red:" + C3.Red + " Green:" + C3.Green + " Blue:" + C3.Blue);
+
+            System.Console.WriteLine();
+
+            //Cookie 4
+            Colour C4 = new Colour(255, 51, 51);
+            Cookie ck4 = new Cookie (80,"Square", C4);
+            System.Console.WriteLine("Cookie 4");
+            System.Console.WriteLine("Weight: " + ck4.Weight + ", Shape: " + ck4.Shape + ", Colour: " + "Red:" + C4.Red + " Green:" + C4.Green + " Blue:" + C4.Blue);
+
+            System.Console.WriteLine();
+
+        }
+    }
+
+    //---------------------------------------------------------
+
+    class Cookie{
+        //atributes
+        public int Weight;
+        public string Shape;
+        public Colour CLR;
+
+        // constructors
+        public Cookie() {
+         this.Weight = 0;
+         this.Shape = "";
+         this.CLR = null;
+        }
+        public Cookie(int weight, string shape, Colour c) {
+            this.Weight = weight;
+            this.Shape = shape;
+            this.CLR = c;
+        }
+    }
+
+    class Colour{
+        public int Red;
+        public int Green;
+        public int Blue;
+
+        public Colour() {
+            this.Red = 0;
+            this.Green = 0;
+            this.Blue = 0;
+        }
+
+        public Colour(int red, int green, int blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes_Task2_DanDavison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate a Random ID
+            Random random1 = new Random();
+            int ID1  = random1.Next(1000, 9999);
+            Random random2 = new Random();
+            int ID2  = random2 .Next(1000, 9999);
+            Random random3 = new Random();
+            int ID3  = random3.Next(1000, 9999);
+
+            //Instantiate a Subject
+            Subject IT = new Subject("Information Technology", 2017);
+            Subject GD = new Subject("Graphic Design", 2013);
+            Subject SM = new Subject("Screen & Media", 2019);
+
+            //Instantiate a Person
+            Person P1 = new Person(ID1, "Dan", "Davison", new DateTime (2000, 01, 01), 180, GD);
+            Person P2 = new Person(ID2, "Luke", "Baker", new DateTime (1998, 07, 03), 170, SM);
+            Person P3 = new Person(ID3, "Caleb", "Parker", new DateTime (2001, 02, 09), 185, IT);
+
+            //Write Height Differences
+            System.Console.WriteLine();
+            P1.GetHeightDifference(P2);
+            P3.GetHeightDifference(P2);
+            P1.GetHeightDifference(P3);
+
+            //Write Subjects
+            System.Console.WriteLine();
+            P1.GetSubject();
+            System.Console.WriteLine();
+            P2.GetSubject();
+            System.Console.WriteLine();
+            P3.GetSubject();
+            System.Console.WriteLine();
+        }
+    }
+    //---------------------------------------------------------
+    public class Person
+    {
+        //atributes
+        public int ID;
+        public string firstname;
+        public string lastname;
+        public DateTime dob;
+        public int height;
+        public Subject s;
+
+        // constructors
+        public Person(int id, string first, string last, DateTime dob, int height, Subject S) {
+            this.ID = id;
+            this.firstname = first;
+            this.lastname = last;
+            this.dob = dob;
+            this.height = height;
+            this.s = S;
+        }
+
+        //Methods
+        public void GetFullName() {
+            System.Console.Write(firstname + " " + lastname);
+        }
+
+        public void GetFullInfo() {
+            System.Console.WriteLine("ID: " + ID + " Name: " + firstname + " " + lastname + " DOB: " + dob + " Height: " + height + "cm");
+        }
+
+        public void GetHeightDifference(Person other) {
+            int difference = this.height - other.height;
+            if(difference > 0){
+                System.Console.WriteLine(this.firstname + " is taller than " + other.firstname + " by " + difference + " cm");
+            }
+            else if(difference < 0){
+                System.Console.WriteLine(this.firstname + " is shorter than " + other.firstname + " by " + difference + " cm");
+            }
+            else {
+                System.Console.WriteLine(this.firstname + " is the same height as " + other.firstname);
+            }
+        }
+
+        public void GetSubject() {
+            System.Console.WriteLine(firstname + " " + lastname + " studied the subject: " + s.name + " in the year: " + s.year);
+        }
+    }
+
+    //---------------------------------------------------------
+    public class Subject
+    {
+        //atributes
+        public string name;
+        public int year;
+
+        // constructors
+        public Subject(string name, int year) {
+            this.name = name;
+            this.year = year;
+        }
+    }    
+}
